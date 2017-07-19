894065c82435213d80c05f5dc47daec3fb43d227
erose357 <erose357@gmail.com>
2017-07-19 13:31:56 -0600

    Add user new, create, show

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+  username "Al"
+  email "al@email.com"
+  password "password"
+  role 0 
+  end
+end
---
+++ spec/features/users/user_creates_account_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "User creates a new account" do
+  scenario "with valid attributes" do
+    user_attributes = {username: "charliem",
+                       password: "cmpassword",
+                       email: "charliem@email.com"}
+
+    visit new_user_path
+
+    fill_in "user[username]", with: user_attributes[:username]
+    fill_in "user[password]", with: user_attributes[:password]
+    fill_in "user[email]", with: user_attributes[:email]
+
+    click_on "Create Account"
+
+    user = User.last
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content("Welcome, #{user.username}")
+    #expect(page).to have_content("Successful login")
+  end
+end
