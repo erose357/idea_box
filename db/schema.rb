13d77d5979d250868edd1ba348a29e87fec7e87e
erose357 <erose357@gmail.com>
2017-07-20 20:23:36 -0600

    Add category_id to ideas table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721011327) do
+ActiveRecord::Schema.define(version: 20170721022016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170721011327) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_ideas_on_category_id"
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
@@ -39,5 +41,6 @@ ActiveRecord::Schema.define(version: 20170721011327) do
     t.integer "role", default: 0
   end
 
+  add_foreign_key "ideas", "categories"
   add_foreign_key "ideas", "users"
 end
